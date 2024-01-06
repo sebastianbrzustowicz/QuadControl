df1bf947f6d2594746214318ef172cfc66cfd570
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-01-06 20:11:11 +0100

    adding todo comments

--- src/main/java/org/sebastianbrzustowicz/Main.java
+++ src/main/java/org/sebastianbrzustowicz/Main.java
@@ -1,5 +1,7 @@
 package org.sebastianbrzustowicz;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -10,22 +12,22 @@ public class Main {
         // Initialisation of the asynchronous listener to the websocket
         WebSocketListener webSocketListener = new WebSocketListener();
         webSocketListener.startListening();
-
         // Initialisation of the ScheduledExecutorService for the calculation thread every 2 ms
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(new ComputationTask(), 0, 2, TimeUnit.MILLISECONDS);
+        //ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        //scheduler.scheduleAtFixedRate(new ComputationTask(), 0, 2, TimeUnit.MILLISECONDS);
     }
 
     static class WebSocketListener {
         // Implementation of an asynchronous listener to the websocket
         // hardcode vehicle ID here
-        private String vehicleID = "96907e09-c4ea-4b1f-8f24-b0d0c9b77687";
+        private final String vehicleID = "96907e09-c4ea-4b1f-8f24-b0d0c9b77687";
         // socket connection url for STOMP protocol
-        private String url_socket = "http://localhost:8080/websocket-endpoint";
+        private final String url_socket = "http://localhost:8080/websocket-endpoint";
         // connection status
-        private String url_status = "http://localhost:8080/topic/vehicle-status/" + vehicleID;
+        private final String url_status = "http://localhost:8080/topic/vehicle-status/" + vehicleID;
         // get actual data
-        private String url_data = "http://localhost:8080/topic/vehicle-data/" + vehicleID;
+        private final String url_data = "http://localhost:8080/topic/vehicle-data/" + vehicleID;
+
         public void startListening() {
             // Here implement the logic for listening for events from the websocket
             // For example, using a library to support WebSocket (e.g. Jetty WebSocket API).
@@ -46,9 +48,12 @@ public class Main {
         @Override
         public void run() {
             // Code for handling calculations on data every 2 ms
-            i += 1;
-            System.out.println("Wartość zmiennej i: " + i);
-            //System.out.println("Wykonywanie obliczeń na danych...");
+
+            // Read task and sensors
+
+            // Compute output
+
+            // GPIO output
         }
     }
 }
