c48715c4358e9159efe1493367894677d338b95b
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-01-12 21:01:30 +0100

    WebSocket implementation

--- src/main/java/org/sebastianbrzustowicz/Main.java
+++ src/main/java/org/sebastianbrzustowicz/Main.java
@@ -1,10 +1,19 @@
 package org.sebastianbrzustowicz;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+
+import java.net.URI;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 
 public class Main {
 
@@ -12,31 +21,51 @@ public class Main {
         // Initialisation of the asynchronous listener to the websocket
         WebSocketListener webSocketListener = new WebSocketListener();
         webSocketListener.startListening();
+
         // Initialisation of the ScheduledExecutorService for the calculation thread every 2 ms
-        //ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        //scheduler.scheduleAtFixedRate(new ComputationTask(), 0, 2, TimeUnit.MILLISECONDS);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(new ComputationTask(), 0, 2, TimeUnit.MILLISECONDS);
     }
 
     static class WebSocketListener {
-        // Implementation of an asynchronous listener to the websocket
-        // hardcode vehicle ID here
-        private final String vehicleID = "96907e09-c4ea-4b1f-8f24-b0d0c9b77687";
-        // socket connection url for STOMP protocol
-        private final String url_socket = "http://localhost:8080/websocket-endpoint";
-        // connection status
-        private final String url_status = "http://localhost:8080/topic/vehicle-status/" + vehicleID;
-        // get actual data
-        private final String url_data = "http://localhost:8080/topic/vehicle-data/" + vehicleID;
+        private final String vehicleID = "e218e18c-9e1c-11ee-8c90-0242ac120002";
+        private final String urlSocket = "http://localhost:8080/websocket-endpoint";
+        private final String urlStatus = "http://localhost:8080/topic/vehicle-status/" + vehicleID;
+        private final String urlData = "http://localhost:8080/topic/vehicle-data/" + vehicleID;
 
         public void startListening() {
-            // Here implement the logic for listening for events from the websocket
-            // For example, using a library to support WebSocket (e.g. Jetty WebSocket API).
-            // CompletableFuture.runAsync(() -> {
-            //    while (true) {
-            //        // Handle websocket events asynchronously here
-            //    }
-            // });
-            // Remember to handle errors and close down resources appropriately.
+            try {
+                WebSocketClient client = new WebSocketClient();
+                client.start();
+
+                URI uri = new URI(urlSocket);
+                ClientUpgradeRequest request = new ClientUpgradeRequest();
+                client.connect(new YourSocketListener(), uri, request);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+
+        @WebSocket
+        public class YourSocketListener {
+
+            @OnWebSocketConnect
+            public void onConnect(Session session) {
+                System.out.println("Connected to WebSocket!");
+                // Handle connection logic, if needed
+            }
+
+            @OnWebSocketMessage
+            public void onMessage(String message) {
+                System.out.println("Received message: " + message);
+                // Handle incoming messages
+            }
+
+            @OnWebSocketClose
+            public void onClose(int statusCode, String reason) {
+                System.out.println("Connection closed: " + statusCode + ", " + reason);
+                // Handle close logic, if needed
+            }
         }
     }
 
@@ -47,6 +76,7 @@ public class Main {
         private int i = 0;
         @Override
         public void run() {
+            //System.out.println("Computations");
             // Code for handling calculations on data every 2 ms
 
             // Read task and sensors
