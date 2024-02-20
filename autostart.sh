25b4bf8f56a132b6a5ffc1f873ae920f65d42e50
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-02-20 23:14:31 +0100

    adding collision detection ai model as a service

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker pull tensorflow/serving
+
+docker stop collision-detection-ai
+docker rm collision-detection-ai
+
+docker run -p 8501:8501 -d --name=collision-detection-ai --mount type=bind,source="$(pwd)"/tfmodel,target=/models/tfmodel/1 -e MODEL_NAME=tfmodel -t tensorflow/serving
+
+mvn clean install
+
+mvn spring-boot:run
