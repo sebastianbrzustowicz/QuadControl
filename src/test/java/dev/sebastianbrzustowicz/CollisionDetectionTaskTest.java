188bea68238d296762406e154bb6ba0f410a8ef4
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-03-07 21:30:48 +0100

    adding execution time test case

--- src/test/java/dev/sebastianbrzustowicz/CollisionDetectionTaskTest.java
+++ src/test/java/dev/sebastianbrzustowicz/CollisionDetectionTaskTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertTrue;
 
 public class CollisionDetectionTaskTest {
@@ -33,6 +35,25 @@ public class CollisionDetectionTaskTest {
         String floatText = printedOutput.split("\\[\\[")[1].split("\\]")[0].trim();
         System.out.println(floatText);
         float extractedValue = Float.parseFloat(floatText);
+
+        assertTrue(Float.isFinite(extractedValue));
+    }
+
+    @Test
+    public void testExecutionTime() {
+        CollisionDetectionTask collisionDetectionTask = new CollisionDetectionTask();
+        collisionDetectionTask.run();
+
+        String printedOutput = outContent.toString().trim();
+        String expectedExpression1 = "Execution time of collision detection request: ";
+        String expectedExpression2 = " microseconds";
+
+        assertTrue(printedOutput.contains(expectedExpression1));
+        assertTrue(printedOutput.contains(expectedExpression2));
+
+        String longText = printedOutput.split(expectedExpression1)[1].split(expectedExpression2)[0].trim();
+
+        float extractedValue = Float.parseFloat(longText);
 
         assertTrue(Float.isFinite(extractedValue));
     }
